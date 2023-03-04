955e32b053b9e7d75322c9358e9d0bc5707dfbb9
007Miles <milendrafernando@gmail.com>
2023-03-04 09:38:11 +0530

    Made an HTTP request

--- app.js
+++ app.js
@@ -18,3 +18,18 @@ http
     res.end();
   })
   .listen(8080);
+
+const https = require("https");
+https
+  .get("https://jsonplaceholder.typicode.com/posts/1", (resp) => {
+    let data = "";
+    resp.on("data", (chunk) => {
+      data += chunk;
+    });
+    resp.on("end", () => {
+      console.log(JSON.parse(data));
+    });
+  })
+  .on("error", (err) => {
+    console.log("Error: " + err.message);
+  });
