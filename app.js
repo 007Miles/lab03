e9316bd726cf662013220b8c84cc4616f1d7e724
007Miles <milendrafernando@gmail.com>
2023-03-04 09:32:12 +0530

    Created write method

--- app.js
+++ app.js
@@ -3,3 +3,9 @@ fs.readFile("file.txt", "utf8", function (err, data) {
   if (err) throw err;
   console.log(data);
 });
+
+const ws = require("fs");
+ws.writeFile("file.txt", "Hello World!", function (err) {
+  if (err) throw err;
+  console.log("File saved!");
+});
